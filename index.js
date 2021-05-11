f3c3342becd47535224486c8fb68967976fcc536
KenzoBT <jkb1015@sru.edu>
2021-05-11 14:24:32 -0400

    Added memory cells

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const http = require('http')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server)
+const path = require('path')
 
 let port = process.env.PORT || 3000
 let counter = 0
+let cells = []
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   //res.send('<h1>Hello, World!</h1>')
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/public/index.html')
 })
 
 io.on('connection', (socket) => {
@@ -18,11 +21,18 @@ io.on('connection', (socket) => {
   console.log('a user connected')
 
   socket.emit('add counter', counter)
+  socket.emit('refresh cells', cells)
 
   socket.on('add counter', () => {
     //console.log('Counter++ request received')
     counter = counter + 1
+    cells.unshift(counter)
+    if(cells.length > 5){
+      cells.pop()
+    }
+    console.log(cells)
     io.emit('add counter', counter)
+    io.emit('refresh cells', cells)
   })
 
   socket.on('disconnect', () => {
